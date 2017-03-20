0ca6a90e85c19b26bcbe95be99801bc566136627
rmody3 <rmody3@gmail.com>
2017-03-20 13:09:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+function fatBastard() {
+  function whatsForDinner() {
+    if (meal ==='Mini-Me'){
+      console.log("The wee man is in my belly!")
+    } else {
+      console.console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+  return whatsForDinner
+}
